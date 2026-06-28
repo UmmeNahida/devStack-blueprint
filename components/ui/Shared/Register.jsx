8290fbc3c185e5ae8ea6bfde6dcdc22f9d3c52b0
Nahida Akther <ummenahida20@gmail.com>
2026-06-28 23:23:36 +0600

    refactor: improve formatting and structure of Register component

--- components/ui/Shared/Register.jsx
+++ components/ui/Shared/Register.jsx
@@ -1,52 +1,51 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export default function Form() {
   const [person, setPerson] = useState({
-    firstName: 'Barbara',
-    lastName: 'Hepworth',
-    email: 'bhepworth@sculpture.com'
+    firstName: "Barbara",
+    lastName: "Hepworth",
+    email: "bhepworth@sculpture.com",
   });
 
   function handleChange(e) {
     setPerson({
       ...person,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   }
 
   return (
-    
-    <div className='grid grid-cols-1 justify-center items-center'>
-      <label>
-        First name:
-        <input
-          className='py-2 px-1 outline-1 border border-gray-500 w-full'
-          name="firstName"
-          value={person.firstName}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Last name:
-        <input
-          name="lastName"
-          value={person.lastName}
-          onChange={handleChange}
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          name="email"
-          value={person.email}
-          onChange={handleChange}
-        />
-      </label>
-      <p>
-        {person.firstName}{' '}
-        {person.lastName}{' '}
-        ({person.email})
-      </p>
+    <div className="flex items-center justify-center">
+      <div className=" w-6/12 mx-auto bg-amber-100 p-3 grid grid-cols-1 justify-center items-center">
+        <label>
+          First name:
+          <input
+            className="py-2 px-1 outline-1 border border-gray-500 w-full"
+            name="firstName"
+            value={person.firstName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Last name:
+          <input
+            name="lastName"
+            value={person.lastName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            name="email"
+            value={person.email}
+            onChange={handleChange}
+          />
+        </label>
+        <p>
+          {person.firstName} {person.lastName} ({person.email})
+        </p>
+      </div>
     </div>
   );
 }
