f99f360a2ddd2e715402d851e7c2d8f55ed201c5
Nahida Akther <ummenahida20@gmail.com>
2026-06-28 23:19:58 +0600

    feat: add register page

---
+++ components/ui/Shared/Register.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+
+export default function Form() {
+  const [person, setPerson] = useState({
+    firstName: 'Barbara',
+    lastName: 'Hepworth',
+    email: 'bhepworth@sculpture.com'
+  });
+
+  function handleChange(e) {
+    setPerson({
+      ...person,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  return (
+    
+    <div className='grid grid-cols-1 justify-center items-center'>
+      <label>
+        First name:
+        <input
+          className='py-2 px-1 outline-1 border border-gray-500 w-full'
+          name="firstName"
+          value={person.firstName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Last name:
+        <input
+          name="lastName"
+          value={person.lastName}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          name="email"
+          value={person.email}
+          onChange={handleChange}
+        />
+      </label>
+      <p>
+        {person.firstName}{' '}
+        {person.lastName}{' '}
+        ({person.email})
+      </p>
+    </div>
+  );
+}
